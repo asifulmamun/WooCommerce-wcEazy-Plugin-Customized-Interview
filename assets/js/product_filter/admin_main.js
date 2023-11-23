580879cbcceff914f3875b0296841d8d2cbd0662
asifulmamun <asifulmamun@gmail.com>
2023-11-24 00:35:43 +0600

    storing success

--- assets/js/product_filter/admin_main.js
+++ assets/js/product_filter/admin_main.js
@@ -32,13 +32,77 @@ function wceazy_product_filter_tab_init(){
 
 
 
-function wceazy_product_filter_save(updatedOrder) {
+
+
+
+
+/* 
+    * Sequence of filte
+    * AL MAMUN
+    * hello@asifulmamun.info.bd
+*/
+// console.log('hello this is admin');
+
+var wceazy_seq_temp = null;
+(function($) {
+  $(function() {
+    var customOrder = {
+      'search': 3,
+      'price': 2,
+      'rating': 1,
+      'category': 4,
+      'stock': 5
+    };
+
+    var sortableList = $("#sortable");
+
+    // Get the list items and sort them based on the customOrder values
+    var items = sortableList.children('li').detach().toArray();
+    items.sort(function(a, b) {
+      var aValue = customOrder[$(a).data('item')];
+      var bValue = customOrder[$(b).data('item')];
+      return aValue - bValue;
+    });
+
+    // Append the sorted list items to the sortable list
+    sortableList.append(items);
+
+    // Make the list sortable
+    sortableList.sortable({
+      update: function(event, ui) {
+        var updatedOrder = {};
+        sortableList.children('li').each(function(index) {
+          var itemName = $(this).data('item');
+          updatedOrder[itemName] = index + 1;
+        });
+
+
+        // Log the updated object to the console
+        // console.log(updatedOrder);
+        // wceazy_seq_temp.push(updatedOrder);
+          wceazy_seq_temp = updatedOrder;
+        
+      }
+    });
+
+    // check null or default
+    if(wceazy_seq_temp == null){
+      console.log('Sequence temp was null so update default');
+      wceazy_seq_temp = customOrder;
+    }
+
+  });
+
+})(jQuery);
+
+
+function wceazy_product_filter_save() {
     jQuery('.wceazy_product_filter_bottom_button_section button').text('Please Wait..');
     jQuery('.wceazy_product_filter_bottom_button_section button').prop('disabled', true);
     let jQuerypostData = {
 
         // sequence filter data
-        'updatedOrder': updatedOrder,
+        'updatedOrder': wceazy_seq_temp,
 
         'show_search_filter': jQuery(".wceazy_product_filter_show_search_filter input[type='checkbox']:checked").length > 0 ? "yes" : "no",
         'show_price_filter': jQuery(".wceazy_product_filter_show_price_filter input[type='checkbox']:checked").length > 0 ? "yes" : "no",
@@ -79,6 +143,7 @@ function wceazy_product_filter_save(updatedOrder) {
 
     };
 
+    // print
     console.log(jQuerypostData);
 
     let jQuerypost_data = {'action': 'wceazy_product_filter_save', 'data': jQuerypostData};
@@ -102,63 +167,4 @@ function wceazy_product_filter_save(updatedOrder) {
 
 
 
-/* 
-    * Sequence of filte
-    * AL MAMUN
-    * hello@asifulmamun.info.bd
-*/
-// console.log('hello this is admin');
-(function($) {
-    $(function() {
-      var customOrder = {
-        'search': 3,
-        'price': 2,
-        'rating': 1,
-        'category': 4,
-        'stock': 5
-      };
-  
-      var sortableList = $("#sortable");
-  
-      function sortItems() {
-        var items = sortableList.children('li').detach().toArray();
-        items.sort(function(a, b) {
-          var aValue = customOrder[$(a).data('item')];
-          var bValue = customOrder[$(b).data('item')];
-          return aValue - bValue;
-        });
-        sortableList.append(items);
-      }
-  
-      sortItems();
-  
-      sortableList.sortable({
-        update: function(event, ui) {
-          // Sorting logic
-        }
-      }).disableSelection();
-  
-      // Handle click on save button
-      var isSaving = false; // Flag to prevent double click
-      $('.wceazy_product_filter_bottom_button_section button').on('click', function() {
-        if (isSaving) return; // If already saving, do nothing
-        isSaving = true; // Set saving flag
-  
-        var updatedOrder = {};
-        sortableList.children('li').each(function(index) {
-          var itemName = $(this).data('item');
-          updatedOrder[itemName] = index + 1;
-        });
-  
-        wceazy_product_filter_save(updatedOrder); // Pass the updatedOrder to the save function
-  
-        sortableList.sortable('disable'); // Temporarily disable sorting during saving
-  
-        setTimeout(function() {
-          sortItems(); // Resort the list items after saving
-          sortableList.sortable('enable'); // Re-enable sorting after saving
-          isSaving = false; // Reset the saving flag
-        }, 500); // Adjust this timeout according to your needs
-      });
-    });
-  })(jQuery);
+
