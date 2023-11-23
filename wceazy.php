9a7345fb2d3e9c1591417ed9e91f1af90cee14da
asifulmamun <asifulmamun@gmail.com>
2023-11-23 18:51:17 +0600

    downloaded

---
+++ wceazy.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plugin Name:       wcEazy
+ * Plugin URI:        https://wceazy.com
+ * Description:       wcEazy provides multiple WooCommerce extensions in a single package you'll ever require.
+ * Version:           1.1.3
+ * Author:            wcEazy
+ * Author URI:        https://wceazy.com
+ * License:           GPL-2.0+
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wceazy
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
+
+define('WCEAZY_VERSION', '1.1.3');
+defined('WCEAZY_PATH') or define('WCEAZY_PATH', plugin_dir_path(__FILE__));
+defined('WCEAZY_URL') or define('WCEAZY_URL', plugin_dir_url(__FILE__));
+defined('WCEAZY_BASE_FILE') or define('WCEAZY_BASE_FILE', __FILE__);
+defined('WCEAZY_BASE_PATH') or define('WCEAZY_BASE_PATH', plugin_basename(__FILE__));
+defined('WCEAZY_IMG_DIR') or define('WCEAZY_IMG_DIR', plugin_dir_url(__FILE__) . 'assets/img/');
+defined('WCEAZY_CSS_DIR') or define('WCEAZY_CSS_DIR', plugin_dir_url(__FILE__) . 'assets/css/');
+defined('WCEAZY_JS_DIR') or define('WCEAZY_JS_DIR', plugin_dir_url(__FILE__) . 'assets/js/');
+defined('WCEAZY_HELP_PAGE') or define('WCEAZY_HELP_PAGE', "https://wceazy.com/contact");
+defined('WCEAZY_DOCS_PAGE') or define('WCEAZY_DOCS_PAGE', "https://wceazy.com/docs");
+defined('WCEAZY_GET_PRO_URL') or define('WCEAZY_GET_PRO_URL', "https://wceazy.com");
+
+function wceazy_check_premium_activation()
+{
+    if (!in_array('wceazy-pro/wceazy.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+
+        require_once WCEAZY_PATH . 'includes/WcEazyUtils.php';
+        require_once WCEAZY_PATH . 'includes/WcEazySettings.php';
+
+        require_once WCEAZY_PATH . 'backend/class-wceazy-ajax.php';
+        require_once WCEAZY_PATH . 'backend/class-wceazy-admin.php';
+
+        require_once WCEAZY_PATH . 'frontend/class-wceazy-ajax.php';
+        require_once WCEAZY_PATH . 'frontend/class-wceazy-client.php';
+
+    }
+}
+add_action('wceazy_pro_check_init', 'wceazy_check_premium_activation', 10, 2);
+do_action('wceazy_pro_check_init');
